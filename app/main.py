9ee281e5eea3b937dc24abe8d1391dbdcfeb2336
asmafrija <asmafrija@hotmail.fr>
2022-04-15 07:23:55 -0700

    added new

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from turtle import title
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import Settings
-from tkinter import *
+import tkinter as tk
 
 # postgres
 from . import models
